f0215795d00f79e0ab3e4f1c68ee89db39164db4
Bruce Banfield <brucebanfield96@hotmail.com>
2023-02-16 10:48:03 -0400

    Finished working

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,11 +1,69 @@
 import React from 'react'
+import TodoList from './TodoList'
+import Form from './Form'
 
 export default class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      todos: [
+        {
+          name: 'Organize Garage',
+          id: 1528817077286, // could look different, you could use a timestamp to generate it
+          completed: false
+        },
+        {
+          name: 'Bake Cookies',
+          id: 1528817084358,
+          completed: false
+        }
+      ]
+    }
+  }
+
+  handleAdd = (name) => {
+
+    const newTodo = {
+      name: name,
+      id: Date.now(),
+      completed: false
+
+    };
+    this.setState({
+      ...this.state,
+      todos: [...this.state.todos, newTodo]
+
+    });
+  }
+
+  handleClear = () => {
+    //1. setState
+    //2. loop through all todos
+    //3. remove all todos that have completed  === true
+    //4. save filtered todos to state
+
+    this.setState({
+      ...this.state,
+      todos: this.state.todos.filter(todo => {
+        return (todo.completed === false);
+      })
+    });
+
+  }
+
   render() {
+    const { todos } = this.state;
+    
     return (
       <div>
-        Todo App
+        <h1> To Dos</h1>
+        
+        <TodoList todos={todos} />
+
+        <Form handleAdd={this.handleAdd}/>
+
+        <button onClick={this.handleClear}>Clear</button>
       </div>
-    )
+    );
   }
 }
